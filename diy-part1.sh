fe0f3893d314f9a96f2d09e95727897332ec1250
SamSuu <31979292+SamSuu@users.noreply.github.com>
2021-08-28 08:12:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,4 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git ssr-plus https://github.com/maxlicheng/luci-app-ssr-plus' >>feeds.conf.default
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
